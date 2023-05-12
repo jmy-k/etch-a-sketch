7c78613e21b8e19ef43fcfeb2ba648b847f57b14
jmy-k <myjuliakan@gmail.com>
2023-05-12 16:03:08 -0400

    established hover eventlisteners (mouseenter , mouseleave) added more grids from 4x4 to 16x16 created prompt function for grid size specification
    trying to figure out how to remove child nodes... somehow .removeChild()
    is not working

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
+function colorIn(event){
+    event.target.classList.add('colored');
+}
+
+function colorOff(event){
+    event.target.classList.remove('colored');
+}
+
 const box = document.querySelectorAll('.box');
 box.forEach((div)=>{
-    div.addEventListener("mouseover",(
-        
-    ))
-})
+    div.addEventListener('mouseenter', event =>{
+        colorIn(event);
+    })
+    div.addEventListener('mouseleave', event =>{
+        colorOff(event);
+    })
+});
+
+function gridSizePrompt(){
+    let gridSize = prompt("Enter Grid Size");
+    const grid = document.querySelectorAll('.rows');
+    console.log(grid);
+    if (grid.hasChildNodes()) {
+        grid.removeChild(grid.children[0]);
+      }
+    
+}
+
+const gridSizeButton = document.querySelector('.gridsize');
+gridSizeButton.addEventListener('click',()=>{
+    gridSizePrompt();
+});
